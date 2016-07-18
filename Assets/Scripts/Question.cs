31a04a631f4b073324b4ffeb39717b4473e9ec80
박성민 <ifndefdeadmau5@gmail.com>
2016-07-18 15:30:13 +0900

    Ray 로 충돌감지하여 각 문제 게임 오브젝트의 메서드 실행

---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class Question : MonoBehaviour
+{
+
+    public Vector2 touchPos;
+    public Transform touchTrans;
+    public GameObject circle;
+    public Text debugText;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    public void moveProblem()
+    {
+        Invoke("invokeTransform", 2);
+    }
+
+    void invokeTransform()
+    {
+        gameObject.transform.Translate(Vector3.right * 20);
+    }
+}
---
+++ Assets/Scripts/RayTest.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class RayTest : MonoBehaviour
+{
+    public GameObject bullet;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Debug.DrawRay(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position), transform.forward.normalized, Color.green, 1.0f);
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Transform objectHit = hit.transform;
+                    Debug.Log(objectHit);
+                    objectHit.gameObject.GetComponent<Question>().moveProblem();
+                    // Do something with the object that was hit by the raycast.
+                }
+
+                Fire();
+            }
+        }
+    }
+    void Fire()
+    {
+        Rigidbody bulletClone = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
+        bulletClone.velocity = transform.forward * 3;
+    }
+
+}
